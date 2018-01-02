f0caed6d7e5cb195943daacd4825c5e044ecf461
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-02 17:43:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,4 +9,11 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  post '/checkout' do
+    @item = params[:item]
+    session["item"] = @item
+    @session = session
+    erb :checkout
+  end
 end
